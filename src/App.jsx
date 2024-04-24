00391c597768a4e4a167af65e0dfdb4eac8b35ec
Joel Martin <joelmartinp13@gmail.com>
2024-04-24 09:04:39 +0530

    Joined dashboard and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 
@@ -6,8 +8,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>   
       </Routes>
+
     </>
   );
 }
